6dd6cd1aa10bccffe66916b961e0266a576de0db
pooyan <thepooyan@gmail.com>
2024-12-06 22:57:03 +0330

    nothing!

--- internal/Game/analysis.go
+++ internal/Game/analysis.go
@@ -1,11 +1,20 @@
 package Game
 
-import "strings"
+import (
+	"reflect"
+	"strings"
+)
 
 
 func analyzeResponse(response string) {
 
   words := strings.Fields(response);
   
-  println("len", len(words))
+  println(isViableAction(words[0]))
+}
+
+func isViableAction(str string) bool {
+  val := reflect.ValueOf(Action{})
+  method := val.MethodByName(str)
+  return method.IsValid()
 }
