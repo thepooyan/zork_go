8e264e207c66a17ea875c563d994538053b2ad08
pooyan <thepooyan@gmail.com>
2024-12-08 21:59:55 +0330

    dictionary added

--- internal/Game/analysis.go
+++ internal/Game/analysis.go
@@ -1,32 +1,46 @@
 package Game
 
 import (
+	"encoding/json"
+	"os"
 	"strings"
 )
 
-
 func (g *Game) analyzeResponse(response string) (Action, []string) {
 
-  words := strings.Fields(response);
+	words := strings.Fields(response)
 
-  ActionNames := getActionNames(g);
-  ActionAliases := getActionAliases();
+	ActionNames := getActionNames(g)
+	ActionAliases := getActionAliases()
 
-  if action, exists := ActionNames[words[0]]; exists {
-    return action, words
-  } else if name, exists := ActionAliases[words[0]]; exists {
-    return ActionNames[name], words
-  } else {
-    return g.Unknown, words
-  }
+	if action, exists := ActionNames[words[0]]; exists {
+		return action, words
+	} else if name, exists := ActionAliases[words[0]]; exists {
+		return ActionNames[name], words
+	} else {
+		return g.Unknown, words
+	}
 }
 
 func getActionAliases() map[string]string {
-  return map[string]string{
-    "hello": "hi",
-    "hey": "hi",
-    "nothing": "idol",
-    "stand": "idol",
-  }
-}
+	file, err := os.Open("./internal/Dict/Dict.json")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
 
+	// Decode JSON into a map
+	var commands map[string][]string
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&commands); err != nil {
+		panic(err)
+	}
+
+  result := make(map[string]string)
+  for key, aliases := range commands {
+    for _, alias := range aliases {
+      result[alias] = key;
+    }
+  }
+  return result
+}
